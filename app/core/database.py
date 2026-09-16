5123f478828d6595bb8eda601fccb51b79b4ad3d
Maria <ms3079316@gmail.com>
2026-09-16 21:55:28 +0300

    Create database.py

---
+++ app/core/database.py
@@ -0,0 +1,31 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession
+)
+
+from app.core.config import settings
+
+
+class Database:
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+        )
+
+    async def get_session(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session() as session:
+            yield session
+
+
+database = Database(
+    url=settings.db.url,
+    echo=settings.db.echo,
+)
