12b85bdd5fe22a8e097179f3b9a8091777a5a8bd
Maria <ms3079316@gmail.com>
2026-09-20 23:15:29 +0300

    Update base.py

--- app/repository/base.py
+++ app/repository/base.py
@@ -3,19 +3,23 @@ from typing import (
     Generic,
     TypeVar,
     Callable,
+    cast,
 )
 
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.functions import func
 
 from app.core.exception import (
     DuplicatedError,
     NotFoundError,
 )
+from app.repository.interface import ReadResult
 
 T = TypeVar("T")
 
-class BaseRepositoryABC(Generic[T]):
+class BaseRepository(Generic[T]):
 
     def __init__(
             self,
@@ -25,13 +29,22 @@ class BaseRepositoryABC(Generic[T]):
         self._session_factory = session_factory
         self._model = model
 
+    def _build_stmt_with_filters(self, stmt, **kwargs):
+        """
+        Функция для построения выражения с фильтрами, уникальными для каждой сущности
+        :param stmt: select выражение
+        :param kwargs: словарь с фильтрами
+        :return: выражение с добавленными фильтрами
+        """
+        return stmt
+
     async def read_by_options(
             self,
-            schema: T | None = None,
             page: int = 1,
             page_size: int = 10,
             ordering: str = "-id",
-    ) -> dict:
+            **kwargs,
+    ) -> ReadResult[T]:
         """
             Возвращает:
             {
@@ -39,14 +52,33 @@ class BaseRepositoryABC(Generic[T]):
                 "total_count": int,
             }
         """
-        ...
+        async with self._session_factory() as session:
+            stmt = select(self._model)
+            stmt = self._build_stmt_with_filters(stmt, **kwargs)
+
+            order_field = ordering.lstrip("-")
+            order_col = getattr(self._model, order_field, None) or self._model.id
+            stmt = stmt.order_by(
+                order_col.desc() if ordering.startswith("-") else order_col.asc(),
+            )
+
+            stmt_paginated = stmt.limit(page_size).offset((page - 1) * page_size)
+            results = await session.execute(stmt_paginated)
+            founds = results.scalars().unique().all()
+            founds: list[T] = cast(list[T], founds)
+
+            count_stmt = select(func.count()).select_from(self._model)
+            count_stmt = self._build_stmt_with_filters(count_stmt, **kwargs)
+            total_count = (await session.execute(count_stmt)).scalar() or 0
+
+        return {"founds": founds, "total_count": total_count}
 
     async def read_by_id(self, id: int) -> T | None:
         async with self._session_factory() as session:
-            result = await session.execute(self._model.get(id=id))
+            stmt = select(self._model).where(self._model.id == id)
+            result = await session.execute(stmt)
 
         return result.scalars().first()
-
 
     async def create(self, schema: T) -> T:
         async with self._session_factory() as session:
@@ -54,6 +86,7 @@ class BaseRepositoryABC(Generic[T]):
             try:
                 session.add(obj)
                 await session.commit()
+                await session.refresh(obj)
             except IntegrityError:
                 await session.rollback()
                 raise DuplicatedError(
@@ -61,12 +94,13 @@ class BaseRepositoryABC(Generic[T]):
                 )
             return obj
 
-    async def update(self, id: int, schema: T) -> T:
+    async def update(self, id: int, schema: T) -> T | None:
         async with self._session_factory() as session:
-            result = await session.execute(self._model.get(id=id))
+            stmt = select(self._model).where(self._model.id == id)
+            result = await session.execute(stmt)
             obj = result.scalars().first()
             if obj is None:
-                raise NotFoundError(detail=f"Record with this id({id}) does not exist.")
+                return None
 
             data = schema.model_dump(exclude_none=True)
             for k, v in data.items():
@@ -78,7 +112,8 @@ class BaseRepositoryABC(Generic[T]):
 
     async def delete_by_id(self, id: int) -> None:
         async with self._session_factory() as session:
-            result = await session.execute(self._model.get(id=id))
+            stmt = select(self._model).where(self._model.id == id)
+            result = await session.execute(stmt)
             obj = result.scalars().first()
             if obj is None:
                 raise NotFoundError(detail=f"Record with this id({id}) does not exist.")
