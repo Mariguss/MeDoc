c808270f7087ff7a522677a72a39307fe0e1b4a6
Maria <ms3079316@gmail.com>
2026-09-20 18:50:15 +0300

    to async version

--- app/service/base.py
+++ app/service/base.py
@@ -12,20 +12,20 @@ class BaseService(Generic[T]):
     def __init__(self, repository) -> None:
         self._repository = repository
 
-    def get_list(self, schema: T | None = None) -> list[T]:
-        return self._repository.read_by_options(schema)
+    async def get_list(self, schema: T | None = None) -> list[T]:
+        return await self._repository.read_by_options(schema)
 
-    def get_by_id(self, id: int) -> T | None:
-        item: T | None = self._repository.read_by_id(id)
+    async def get_by_id(self, id: int) -> T | None:
+        item: T | None = await self._repository.read_by_id(id)
         if item is None:
             return None
         return item
 
-    def add(self, schema: T) -> T:
-        return self._repository.create(schema)
+    async def add(self, schema: T) -> T:
+        return await self._repository.create(schema)
 
-    def patch(self, id: int, schema: T) -> T:
-        return self._repository.update(id, schema)
+    async def patch(self, id: int, schema: T) -> T:
+        return await self._repository.update(id, schema)
 
-    def remove_by_id(self, id: int) -> None:
-        return self._repository.delete_by_id(id)
+    async def remove_by_id(self, id: int) -> None:
+        return await self._repository.delete_by_id(id)
--- app/service/interface.py
+++ app/service/interface.py
@@ -12,21 +12,21 @@ T = TypeVar("T")
 class BaseServiceABC(Generic[T], ABC):
 
     @abstractmethod
-    def get_list(self, schema: T | None = None) -> list[T]:
+    async def get_list(self, schema: T | None = None) -> list[T]:
         ...
 
     @abstractmethod
-    def get_by_id(self, id: int) -> T | None:
+    async def get_by_id(self, id: int) -> T | None:
         ...
 
     @abstractmethod
-    def add(self, schema: T) -> T:
+    async def add(self, schema: T) -> T:
         ...
 
     @abstractmethod
-    def patch(self, id: int, schema: T) -> T:
+    async def patch(self, id: int, schema: T) -> T:
         ...
 
     @abstractmethod
-    def remove_by_id(self, id: int) -> None:
+    async def remove_by_id(self, id: int) -> None:
         ...
